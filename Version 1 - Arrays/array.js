62f8997447b4dd7511b325e58d3eaf2503e26599
Shawn <shawn.kong@accenture.com>
2019-08-27 00:19:44 +0800

    array

--- Version 1 - Arrays/array.js
+++ Version 1 - Arrays/array.js
@@ -1,5 +1,21 @@
+// store
 var todos = ['item 1', 'item 2', 'item 3'];
 
+// display
 console.log(todos);
 console.log("todos");
 console.log("My todos:" + " " + todos);
+
+// add
+todos.push("item 4");
+console.log(todos);
+todos.push("item 5");
+console.log(todos);
+
+// edit
+todos[0] = "ITEM_1";
+console.log(todos);
+
+// delete
+todos.splice(0, 2);
+console.log(todos);
