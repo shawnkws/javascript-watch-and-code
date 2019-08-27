dd9d8477e8580ea8ae4d7924c19fd5a178c454f1
Shawn Kong <shawnkws@gmail.com>
2019-08-27 14:30:51 +0800

    objects

--- Version 4 - Booleans/booleans.js
+++ Version 4 - Booleans/booleans.js
@@ -1,25 +1,28 @@
 var todoObj = {
   // store
-  todos: ["item 1", "item 2", "item 3"],
+  todos: [],
 
   // display
   display: function(obj, text) {
     if (text == undefined || text == null) {
-      console.log("My todos:" + " " + obj);
+      console.log("My todos:", obj);
     } else {
-      console.log(text + "\n" + "My todos:" + " " + obj);
+      console.log(text + "\n" + "My todos:", obj);
     }
   },
 
   // add
   add: function(arrName, objAdd) {
-    arrName.push(objAdd);
+    arrName.push({
+      name: objAdd,
+      completed: false
+    });
     this.display(arrName, "Todo item(s) added.");
   },
 
   // edit
   edit: function(arrName, arrIndex, objEdit) {
-    arrName[arrIndex] = objEdit;
+    arrName[arrIndex].name = objEdit;
     this.display(arrName, "Todo item(s) edited.");
   },
 
@@ -28,16 +31,25 @@ var todoObj = {
     objRemoveCount = isNaN(objRemoveCount) ? 1 : objRemoveCount;
     arrName.splice(arrIndex, objRemoveCount);
     this.display(arrName, "Todo item(s) removed.");
+  },
+
+  // complete
+  toggleStatus: function(arrName, arrIndex) {
+    arrName[arrIndex].completed = !arrName[arrIndex].completed;
+    this.display(arrName, "Todo item(s) status changed.");
   }
 };
 
+/*
 todoObj.display(todoObj.todos);
 
-todoObj.add(todoObj.todos, "item 4");
-todoObj.add(todoObj.todos, "item 5");
+todoObj.add(todoObj.todos, "item 1");
+todoObj.add(todoObj.todos, "item 2");
 
 todoObj.edit(todoObj.todos, 0, "ITEM_1");
-todoObj.edit(todoObj.todos, 4, "ITEM_5");
+todoObj.edit(todoObj.todos, 1, "ITEM_2");
 
-todoObj.remove(todoObj.todos, 4);
-todoObj.remove(todoObj.todos, 0);
+todoObj.remove(todoObj.todos, 1);
+
+todoObj.toggleStatus(todoObj.todos, 0);
+*/
